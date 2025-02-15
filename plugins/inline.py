3c902a220f566b4490479f890eec02157aa30f2d
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-15 18:23:04 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -35,7 +35,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_req_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='😁You have to subscribe my channel to use the bot😁',
                            switch_pm_parameter="subscribe")
         return
 
